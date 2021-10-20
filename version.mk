0149fef3b023df3504d6dc05a710f6216a9f94b8
Enrico Weigelt, metux IT consult <info@metux.net>
2021-10-20 13:06:39 +0200

    add common build scripts
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

---
+++ version.mk
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) Enrico Weigelt, metux IT consult <info@metux.net>
+
+# this file is meant to be shared/synced between many packages (v0.4)
+
+ZIMBRA_VERSION_MAJOR ?= 9
+ZIMBRA_VERSION_MINOR ?= 0
+ZIMBRA_VERSION_MICRO ?= 1
+ZIMBRA_VERSION_PATCH ?= 1984
